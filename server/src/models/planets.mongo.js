4abe6b98e2a881732e1bd9e19925528884c7d6de
Richard Colachagua <richardcolachagua@Richards-MBP.home>
2022-07-05 20:29:24 -0400

    first commit

---
+++ server/src/models/planets.mongo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const planetsSchema = new mongoose.Schema({
+    keplerName: {
+        type: String,
+        required: true,
+    }
+});
+
+// Connects planetSchema with the "planets" collection
+module.exports = mongoose.model('Planet', planetsSchema);
---
+++ server/src/routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const planetsRouter = require('./planets/planets.router');
+const launchesRouter = require('./launches/launches.router');
+
+const api = express.Router();
+
+app.use('/planets', planetsRouter);
+app.use('/launches',launchesRouter);
+
+module.exports = api;
